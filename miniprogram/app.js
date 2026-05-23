ad0d378899dc08bae39565c108c28c8e2ffbdf81
zuolan <1939826046@qq.com>
2026-05-23 14:21:32 +0800

    feat: clean up old code, scaffold waypoint project skeleton

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,19 +1,28 @@
 // app.js
 App({
   onLaunch: function () {
+    const envId = "";
     this.globalData = {
-      // env 参数说明：
-      // env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会请求到哪个云环境的资源
-      // 此处请填入环境 ID, 环境 ID 可在微信开发者工具右上顶部工具栏点击云开发按钮打开获取
-      env: "",
+      env: envId,
+      isCloudReady: false,
+      markerStyle: "game",
     };
     if (!wx.cloud) {
       console.error("请使用 2.2.3 或以上的基础库以使用云能力");
     } else {
-      wx.cloud.init({
-        env: this.globalData.env,
-        traceUser: true,
-      });
+      wx.cloud.init({ env: envId, traceUser: true });
+      this.globalData.isCloudReady = !!envId;
+      if (!envId) {
+        console.warn("云开发环境 ID 未配置，请在 app.js 中填入您的环境 ID");
+      }
     }
+  },
+
+  getDb: function () {
+    return wx.cloud.database();
+  },
+
+  callFunction: function (name, data) {
+    return wx.cloud.callFunction({ name, data });
   },
 });
